0ef436b86813689925df03959fe58835fa3785d3
Sebastian Garcia Helguera <sebastiangarciahelguera@gmail.com>
2023-10-02 13:11:29 -0700

    add more error checking to post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,6 +14,22 @@ if VISIBILITY not in ["public", "private", "internal"]:
     print(f'ERROR: {VISIBILITY} is not a valid visibility for github, it must be one of "public", "private" or "internal"')
     sys.exit(1)
 
+def check_dependencies() -> bool:
+    try:
+        subprocess.run(["git", "--version"], capture_output=True, check=True)
+    except subprocess.CalledProcessError as exception:
+        print(f"ERROR: git failed with exit code: {exception.returncode}. (is git installed?)")
+        return False
+    try:
+        subprocess.run(["gh", "--version"], capture_output=True, check=True)
+        return True
+    except subprocess.CalledProcessError as exception:
+        print(f"ERROR: gh failed with exit code {exception.returncode}. (is gh installed? gh is github CLI)")
+        return False
+
+if not check_dependencies():
+    sys.exit(1)
+
 subprocess.call(["git", "init", "-b", "main"])
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "first commit"])
