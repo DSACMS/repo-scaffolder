ccd76b0e4c5d58f27ec69500bf85e367b4344012
aprilselby88 <a.selby88@gmail.com>
2023-11-04 13:32:18 -0500

    feat: restructure project to multiple cookiecutters

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,3 +1,5 @@
+import sys
+
 CONTRIBUTORS = '{{ cookiecutter.greater_one_contributors }}'
 RELEASE = '{{cookiecutter.greater_one_release}}'
 WORK = '{{cookiecutter.work_as_team}}'
@@ -8,23 +10,29 @@ if CONTRIBUTORS == "no":
     # tier 0
     print("We suggest using the tier 0 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier0")
+    sys.exit(1)
 elif RELEASE == "no":
     #tier 1
     print("We suggest using the tier 1 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier1")
+    sys.exit(1)
 elif WORK == "no":
     #tier 1
     print("We suggest using the tier 1 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier1")
+    sys.exit(1)
 elif MAINTAIN == "no":
     # tier 2
     print("We suggest using the tier 2 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier2")
+    sys.exit(1)
 elif ROADMAP == "no":
     # tier 3
     print("We suggest using the tier 3 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier3")
+    sys.exit(1)
 else:
     # tier 4
     print("We suggest using the tier 4 template for your project.  Run: ")
     print("cookiecutter https://github.com/DSACMS/repo-scaffolder/scaffold-templates/tier4")
+    sys.exit(1)
