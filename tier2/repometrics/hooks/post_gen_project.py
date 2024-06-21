5e4dca7e7be28559fb267a7c0b7175200178becd
Ricardo Zamora <rzgarcia27@gmail.com>
2024-06-21 11:01:59 -0700

    repometrics cookiecutter developed

---
+++ tier2/repometrics/hooks/post_gen_project.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import time 
+
+# Get working directory and repometrics file
+current_dir = os.getcwd()
+repometrics_file = "repometrics.json"
+source_file = os.path.join(current_dir, repometrics_file)
+
+# Get parent directory, one level up from the current directory
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+
+# Construct the destination path in the parent directory
+destination_file = os.path.join(parent_dir, repometrics_file)
+
+# Attempt to move repometrics to parent directory
+try:
+    shutil.move(source_file, destination_file)
+except FileNotFoundError:
+    print(f"The file {source_file} does not exist.")
+except PermissionError:
+    print(f"Permission denied: Unable to move the file {source_file}.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+# Remove directory once repometrics file was successfully moved
+os.chdir("..")
+dir_name = "{{cookiecutter.project_type}}"
+
+try:
+    # Check if the directory exists
+    if os.path.exists(dir_name):
+        # Attempt to remove the directory
+        print(f"Directory '{dir_name}' has been removed.")
+    else:
+        print(f"Directory '{dir_name}' does not exist.")
+except FileNotFoundError:
+    print(f"File not found: The directory '{dir_name}' does not exist.")
+except Exception as e:
+    print(f"An error occurred: {e}")
