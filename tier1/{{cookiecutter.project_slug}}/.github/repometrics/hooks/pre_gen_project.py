be750a0c60c9bd114e4615fc078e5985b4382b39
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2024-09-24 12:32:21 -0500

    add scc as an optional dependency to create default for labor hours
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

---
+++ tier1/{{cookiecutter.project_slug}}/.github/repometrics/hooks/pre_gen_project.py
@@ -0,0 +1,26 @@
+import subprocess
+import shutil
+import json
+from cookiecutter.main import cookiecutter
+
+
+if shutil.which('scc') is not None:
+    try:
+        #Run scc and load results into a dictionary
+        d = json.loads(subprocess.run(["scc", "--format","json2"],check=True, capture_output=True).stdout)
+        
+        #inject a default value for labor hours calculated from scc
+        cookiecutter(
+            'cookiecutter-django',
+            extra_context={'labor_hours': d['estimatedScheduleMonths'] * 730.001}
+        )
+    except subprocess.CalledProcessError as e:
+        print(e)
+else:
+    print("scc (https://github.com/boyter/scc) not found on system")
+
+    #Otherwise just use previous value as a default value.
+    cookiecutter(
+        'cookiecutter-django',
+        extra_context={'labor_hours': 0}
+    )
