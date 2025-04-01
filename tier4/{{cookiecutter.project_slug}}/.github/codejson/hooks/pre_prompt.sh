7c9780c4c15ff1825dcabf474dce65ed0690f1d7
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2025-04-01 16:08:48 -0700

    Updated code.json cookiecutter cli to reflect latest version to Tier 4
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

---
+++ tier4/{{cookiecutter.project_slug}}/.github/codejson/hooks/pre_prompt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "⚙️  Welcome to the code.json cookiecutter CLI."
+echo -e "📝  We will assist you with creating a code.json file to store metadata of your project."
+echo -e "ℹ️  Visit www.github.com/DSACMS/gov-codejson for more information."
