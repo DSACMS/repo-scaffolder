e7d27f2886e30fcf7a3025b9d3a2a7a9b1ff678c
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2025-04-08 09:20:18 -0700

    Added intro text to sorting hat cookiecutter CLI
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

---
+++ hooks/pre_prompt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "👋  Welcome to the repo-scaffolder cookiecutter CLI."
+echo -e "⚙️  We will assist you with creating a repository."
+echo -e "📝  Answer the following questions to determine what maturity model tier your project is classified as:"
+echo -e "ℹ️  Visit https://github.com/DSACMS/repo-scaffolder/blob/main/maturity-model-tiers.md for more information."
