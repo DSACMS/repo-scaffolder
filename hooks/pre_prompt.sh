379218f96c4a541a618240a060bfa428b05df1ef
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2025-04-09 10:16:51 -0700

    Remove cookiecutter functionality for sorting hat
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- hooks/pre_prompt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo -e "👋  Welcome to the repo-scaffolder cookiecutter CLI."
-echo -e "⚙️  We will assist you with creating a repository."
-echo -e "📝  Answer the following questions to identify the maturity model tier of your project."
-echo -e "ℹ️  Visit https://github.com/DSACMS/repo-scaffolder/blob/main/maturity-model-tiers.md for more information."
