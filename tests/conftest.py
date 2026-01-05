c3449f88e891d6dc246d570e50b5f2fe8c8cd08a
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2026-01-05 11:13:52 -0500

    Update conftest.py to include context additions for tier 3-4
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
 
 @pytest.fixture
-def context():
-    return {
+def template_tier(request):
+    template = request.config.getoption("--template")
+    return int(template[-1])
+
+@pytest.fixture
+def context(template_tier):
+    context = {
         "project_name": "Web Dashboard",
         "project_slug": "web_dashboard",
         "project_org": "DSACMS",
@@ -13,6 +18,12 @@ def context():
         "receive_updates": False,
         "add_team": False
     }
+    if template_tier == 3 or template_tier == 4:
+        context.update({
+            "code_owners": "",
+            "add_maintainer": False
+        })
+    return context
 
 @pytest.fixture
 def result(cookies, context):
